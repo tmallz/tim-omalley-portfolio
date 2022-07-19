de223c86358881d03c47c1489a7f693fba06874b
Tim O'Malley <tomalley24@gmail.com>
2022-07-19 11:23:44 -0500

    deploy fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-github';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
@@ -14,6 +14,11 @@ const config = {
       fallback: null,
       precompress: null,
     }),
+    prerender: {
+      default: true,
+      crawl: true,
+      enabled: true,
+    },
   },
   preprocess: [
     preprocess({
