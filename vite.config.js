d42166f42f677fd680e89a0c4ddc379baf15219f
Tim O'Malley <tomalley24@gmail.com>
2022-07-19 15:52:01 -0500

    reverting to old portfolio to fix deploy issues

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-
-/** @type {import('vite').UserConfig} */
-const config = {
-  base: '/tim-omalley-portfolio/',
-  plugins: [sveltekit()],
-};
-
-export default config;
