646412680453740045575ec2f66e93a885a19902
Tim O'Malley <tomalley24@gmail.com>
2022-07-19 08:48:17 -0500

    deploy fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'svelte-adapter-github';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // preprocess: preprocess(),
-
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: true,
+      pages: 'docs',
+      assets: 'docs',
+      domain: null,
+      jekyll: false,
+      fallback: null,
+      precompress: false,
     }),
   },
   preprocess: [
@@ -17,6 +20,12 @@ const config = {
       postcss: true,
     }),
   ],
+  paths: {
+    base: dev ? '' : '/tim-omalley-portfolio',
+  },
+  // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+  // For example, instead of '_app', use 'app_', 'internal', etc.
+  appDir: 'internal',
 };
 
 export default config;
