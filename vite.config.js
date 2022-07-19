d4a6fb529060ee2bce179613774e496b7bc408e0
Tim O'Malley <tomalley24@gmail.com>
2022-07-19 14:58:01 -0500

    deploy fixes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  base: 'tim-omalley-portfolio/',
+  base: '/tim-omalley-portfolio/',
   plugins: [sveltekit()],
 };
 
