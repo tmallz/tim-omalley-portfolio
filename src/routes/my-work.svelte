99b09b0a335b0cc55dcfe2d7fc6e61799f4a04e9
Timothy O'Malley <tomalley24@gmail.com>
2022-10-10 20:51:13 -0500

    changes

--- src/routes/my-work.svelte
+++ src/routes/my-work.svelte
@@ -1,88 +1,128 @@
-<div class="grid min-h-[5rem] card bg-base-300 rounded-box place-items-center">
-  <div class="preview border-base-300 bg-base-200 rounded-box flex min-h-[6rem] min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-8 undefined m-10 md:m-20" style="background-size: 5px 5px">
-    <div class="card lg:card-side bg-base-100 shadow-xl">
-      <figure><img src="images/green-genie.png" alt="Picture of green geenie app"></figure>
-      <div class="card-body">
-        <h2 class="card-title">Green Genie</h2>
-          <p>
-            Green Genie is an app that aims to give any marijauna plant
-            growing enthusiast, a database to log for marijuana strains. It
-            will also give you the location of your local despensaries. My
-            responsibilities on this project were the back end, making sure
-            the routes worked properly, and the authentication both front
-            and back end for that. I also did a bit of the handlebars.js
-            work and some of the styling on the web pages. Click the image
-            to see the deployed application.
-          </p>
-        <div class="card-actions justify-center md:justify-end">        
-          <a href = "https://green-genie5.herokuapp.com/" target = "_blank"><button class="btn btn-primary">View Site</button></a>
-          <a href = "https://github.com/tmallz/GreenGenie" target = "_blank"><button class="btn btn-primary">View Repo</button></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="preview border-base-300 bg-base-200 rounded-box flex min-h-[6rem] min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-8 undefined m-10 md:m-20" style="background-size: 5px 5px">
-    <div class="card lg:card-side bg-base-100 shadow-xl">
-      <figure><img src="images/tech-blog.PNG" alt="Picture of Tech Blog app"></figure>
-      <div class="card-body">
-        <h2 class="card-title">Tech Blog</h2>
-          <p>
-            The Tech Blog is a full stack application that allows to user to
-            interact with with posts. The user can log in or make an
-            account. Once logged in the user has full access to the sites
-            functionality. This includes accessing their dashboard to see
-            all posts they have made, and from there they can edit/delete
-            the posts. The user can also comment on posts made by other
-            users. Click the image to see the deployed application
-          </p>
-          <div class="card-actions justify-center md:justify-end">        
-            <a href = "https://tech-blog-tlo.herokuapp.com/" target = "_blank"><button class="btn btn-primary">View Site</button></a>
-            <a href = "https://github.com/tmallz/tim-omalley-hw-week-14" target = "_blank"><button class="btn btn-primary">View Repo</button></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="preview border-base-300 bg-base-200 rounded-box flex min-h-[6rem] min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-8 undefined m-10 md:m-20" style="background-size: 5px 5px">
-    <div class="card lg:card-side bg-base-100 shadow-xl">
-      <figure><img src="images/weatherdashboard.png" alt="Picture of Weather dashboard app"></figure>
-      <div class="card-body">
-        <h2 class="card-title">Tech Blog</h2>
-          <p>
-            The weather dashboard app provides the user with weather data
-            about a city that they search for. Once the user searches for a
-            city they are provided with the current weather conditions as
-            well as a five day forcast. The app generates a button of past
-            searches for easy access past search results. My
-            responsibilities included the full scope of the project.
-          </p>
-        <div class="card-actions justify-center md:justify-end">
-          <a href="https://tmallz.github.io/Tim-OMalley-Homework-6/" target="_blank"><button class="btn btn-primary">View Site</button></a>
-          <a href="https://github.com/tmallz/Tim-OMalley-Homework-6" target="_blank"><button  class="btn btn-primary">View Repo</button></a>
-        </div>
-      </div>
-    </div>
-  </div>
+<div
+	class="preview border-base-300 bg-base-200 rounded-box flex min-h-[6rem] min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-8 undefined m-10 md:m-20"
+	style="background-size: 5px 5px"
+>
+	<div class="card lg:card-side bg-base-100 shadow-xl">
+		<figure>
+			<img src="images/brewerme.PNG" alt="Picture of BreweryMe app" />
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">BrewerMe</h2>
+			<p>
+				BrewerMe is a full stack application built on Sveltekit, supabase,
+				TailwindCSS, Node.js, and Express. It allows users to input a city and
+				get returned a list of breweries in that city. The user can then click
+				on a brewery to get more information about that brewery. The user can
+				also add the brewery to their favorites list to come back to it later. I
+				plan on implementing a google map that populates the brewery location
+				for user. I would like to implement a beer rating system to the app as
+				well if I have the time. I started the app in svelte vanilla and learned
+				about sveltekit after building out some of the features, I am porting it
+				over to the sveltekit app to allow for easier routing and backend.
+			</p>
+			<div class="card-actions justify-center md:justify-end">
+				<a href="https://brewery-app-ecru.vercel.app/" target="_blank"
+					><button class="btn btn-primary">View Site</button></a
+				>
+				<a href="https://github.com/tmallz/brewery-app" target="_blank"
+					><button class="btn btn-primary">View Repo</button></a
+				>
+			</div>
+		</div>
+	</div>
 </div>
-<div class="divider">~In Progress Below~</div>
-  <div class="grid min-h-[5rem] card bg-base-300 rounded-box place-items-center">
-    <div class="preview border-base-300 bg-base-200 rounded-box flex min-h-[6rem] min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-8 undefined m-10 md:m-20" style="background-size: 5px 5px">
-      <div class="card lg:card-side bg-base-100 shadow-xl">
-      <figure><img src="images/brewerme.PNG" alt="Picture of BreweryMe app"></figure>
-        <div class="card-body">
-          <h2 class="card-title">BrewerMe</h2>
-          <p>
-            BrewerMe is a full stack application built on Sveltekit, SQL, Seqluelize, TailwindCSS, Node.js, and Express. It allows users to input a city and get returned 
-            a list of breweries in that city. The user can then click on a brewery to get more information about that brewery. 
-            The user can also add the brewery to their favorites list to come back to it later. I plan on implementing a google map that populates the brewery location for 
-            user. I would like to implement a beer rating system to the app as well if I have the time. I started the app in svelte vanilla and learned about sveltekit after
-            building out some of the features, I am porting it over to the sveltekit app to allow for easier routing and backend.
-          </p>
-        <div class="card-actions justify-center md:justify-end">        
-        <div class="btn btn-danger">No site yet</div>
-          <a href = "https://github.com/tmallz/brewery-app" target = "_blank"><button class="btn btn-primary">View Repo (sveltekit)</button></a>
-          <a href = "https://github.com/tmallz/svelte-practice" target = "_blank"><button class="btn btn-primary">View Repo (svelte only)</button></a>
-        </div>
-      </div>
-    </div>
-  </div>
+<div
+	class="preview border-base-300 bg-base-200 rounded-box flex min-h-[6rem] min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-8 undefined m-10 md:m-20"
+	style="background-size: 5px 5px"
+>
+	<div class="card lg:card-side bg-base-100 shadow-xl">
+		<figure>
+			<img src="images/green-genie.png" alt="Picture of green geenie app" />
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Green Genie</h2>
+			<p>
+				Green Genie is an app that aims to give any marijauna plant growing
+				enthusiast, a database to log for marijuana strains. It will also give
+				you the location of your local despensaries. My responsibilities on this
+				project were the back end, making sure the routes worked properly, and
+				the authentication both front and back end for that. I also did a bit of
+				the handlebars.js work and some of the styling on the web pages. Click
+				the image to see the deployed application.
+			</p>
+			<div class="card-actions justify-center md:justify-end">
+				<a href="https://green-genie5.herokuapp.com/" target="_blank"
+					><button class="btn btn-primary">View Site</button></a
+				>
+				<a href="https://github.com/tmallz/GreenGenie" target="_blank"
+					><button class="btn btn-primary">View Repo</button></a
+				>
+			</div>
+		</div>
+	</div>
+</div>
+<div
+	class="preview border-base-300 bg-base-200 rounded-box flex min-h-[6rem] min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-8 undefined m-10 md:m-20"
+	style="background-size: 5px 5px"
+>
+	<div class="card lg:card-side bg-base-100 shadow-xl">
+		<figure>
+			<img src="images/tech-blog.PNG" alt="Picture of Tech Blog app" />
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Tech Blog</h2>
+			<p>
+				The Tech Blog is a full stack application that allows to user to
+				interact with with posts. The user can log in or make an account. Once
+				logged in the user has full access to the sites functionality. This
+				includes accessing their dashboard to see all posts they have made, and
+				from there they can edit/delete the posts. The user can also comment on
+				posts made by other users. Click the image to see the deployed
+				application
+			</p>
+			<div class="card-actions justify-center md:justify-end">
+				<a href="https://tech-blog-tlo.herokuapp.com/" target="_blank"
+					><button class="btn btn-primary">View Site</button></a
+				>
+				<a
+					href="https://github.com/tmallz/tim-omalley-hw-week-14"
+					target="_blank"><button class="btn btn-primary">View Repo</button></a
+				>
+			</div>
+		</div>
+	</div>
+</div>
+<div
+	class="preview border-base-300 bg-base-200 rounded-box flex min-h-[6rem] min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-8 undefined m-10 md:m-20"
+	style="background-size: 5px 5px"
+>
+	<div class="card lg:card-side bg-base-100 shadow-xl">
+		<figure>
+			<img
+				src="images/weatherdashboard.png"
+				alt="Picture of Weather dashboard app"
+			/>
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Weather App</h2>
+			<p>
+				The weather dashboard app provides the user with weather data about a
+				city that they search for. Once the user searches for a city they are
+				provided with the current weather conditions as well as a five day
+				forcast. The app generates a button of past searches for easy access
+				past search results. My responsibilities included the full scope of the
+				project.
+			</p>
+			<div class="card-actions justify-center md:justify-end">
+				<a
+					href="https://tmallz.github.io/Tim-OMalley-Homework-6/"
+					target="_blank"><button class="btn btn-primary">View Site</button></a
+				>
+				<a
+					href="https://github.com/tmallz/Tim-OMalley-Homework-6"
+					target="_blank"><button class="btn btn-primary">View Repo</button></a
+				>
+			</div>
+		</div>
+	</div>
 </div>
