b97c78c63062d376910775b869711ce63e9acbd9
Timothy O'Malley <tomalley24@gmail.com>
2023-10-19 09:39:58 -0500

    changes

---
+++ .svelte-kit/runtime/env/static/private.js
@@ -0,0 +1,243 @@
+// this file is generated — do not edit it
+/** @type {import('$env/static/private').ALLUSERSPROFILE} */
+export const ALLUSERSPROFILE = "C:\\ProgramData";
+
+/** @type {import('$env/static/private').APPDATA} */
+export const APPDATA = "C:\\Users\\Tim\\AppData\\Roaming";
+
+/** @type {import('$env/static/private').ChocolateyInstall} */
+export const ChocolateyInstall = "C:\\ProgramData\\chocolatey";
+
+/** @type {import('$env/static/private').ChocolateyLastPathUpdate} */
+export const ChocolateyLastPathUpdate = "133248798071531510";
+
+/** @type {import('$env/static/private').CHROME_CRASHPAD_PIPE_NAME} */
+export const CHROME_CRASHPAD_PIPE_NAME = "\\\\.\\pipe\\LOCAL\\crashpad_11268_TDMQCZOCFMAAICFQ";
+
+/** @type {import('$env/static/private').COLOR} */
+export const COLOR = "1";
+
+/** @type {import('$env/static/private').COLORTERM} */
+export const COLORTERM = "truecolor";
+
+/** @type {import('$env/static/private').CommonProgramFiles} */
+export const CommonProgramFiles = "C:\\Program Files\\Common Files";
+
+/** @type {import('$env/static/private').CommonProgramW6432} */
+export const CommonProgramW6432 = "C:\\Program Files\\Common Files";
+
+/** @type {import('$env/static/private').COMPUTERNAME} */
+export const COMPUTERNAME = "LAPTOP-H4STDHO7";
+
+/** @type {import('$env/static/private').ComSpec} */
+export const ComSpec = "C:\\WINDOWS\\system32\\cmd.exe";
+
+/** @type {import('$env/static/private').DriverData} */
+export const DriverData = "C:\\Windows\\System32\\Drivers\\DriverData";
+
+/** @type {import('$env/static/private').EDITOR} */
+export const EDITOR = "C:\\WINDOWS\\notepad.exe";
+
+/** @type {import('$env/static/private').EFC_6692} */
+export const EFC_6692 = "1";
+
+/** @type {import('$env/static/private').GIT_ASKPASS} */
+export const GIT_ASKPASS = "c:\\Users\\Tim\\AppData\\Local\\Programs\\Microsoft VS Code\\resources\\app\\extensions\\git\\dist\\askpass.sh";
+
+/** @type {import('$env/static/private').HOME} */
+export const HOME = "C:\\Users\\Tim";
+
+/** @type {import('$env/static/private').HOMEDRIVE} */
+export const HOMEDRIVE = "C:";
+
+/** @type {import('$env/static/private').HOMEPATH} */
+export const HOMEPATH = "\\Users\\Tim";
+
+/** @type {import('$env/static/private').INIT_CWD} */
+export const INIT_CWD = "C:\\Users\\Tim\\Documents\\GitHub\\tim-omalley-portfolio";
+
+/** @type {import('$env/static/private').LANG} */
+export const LANG = "en_US.UTF-8";
+
+/** @type {import('$env/static/private').LOCALAPPDATA} */
+export const LOCALAPPDATA = "C:\\Users\\Tim\\AppData\\Local";
+
+/** @type {import('$env/static/private').LOGONSERVER} */
+export const LOGONSERVER = "\\\\LAPTOP-H4STDHO7";
+
+/** @type {import('$env/static/private').NODE} */
+export const NODE = "C:\\Program Files\\nodejs\\node.exe";
+
+/** @type {import('$env/static/private').NODE_EXE} */
+export const NODE_EXE = "C:\\Program Files\\nodejs\\\\node.exe";
+
+/** @type {import('$env/static/private').NPM_CLI_JS} */
+export const NPM_CLI_JS = "C:\\Users\\Tim\\AppData\\Roaming\\npm\\node_modules\\npm\\bin\\npm-cli.js";
+
+/** @type {import('$env/static/private').npm_command} */
+export const npm_command = "run-script";
+
+/** @type {import('$env/static/private').npm_config_cache} */
+export const npm_config_cache = "C:\\Users\\Tim\\AppData\\Local\\npm-cache";
+
+/** @type {import('$env/static/private').npm_config_engine_strict} */
+export const npm_config_engine_strict = "true";
+
+/** @type {import('$env/static/private').npm_config_globalconfig} */
+export const npm_config_globalconfig = "C:\\Users\\Tim\\AppData\\Roaming\\npm\\etc\\npmrc";
+
+/** @type {import('$env/static/private').npm_config_global_prefix} */
+export const npm_config_global_prefix = "C:\\Users\\Tim\\AppData\\Roaming\\npm";
+
+/** @type {import('$env/static/private').npm_config_init_module} */
+export const npm_config_init_module = "C:\\Users\\Tim\\.npm-init.js";
+
+/** @type {import('$env/static/private').npm_config_local_prefix} */
+export const npm_config_local_prefix = "C:\\Users\\Tim\\Documents\\GitHub\\tim-omalley-portfolio";
+
+/** @type {import('$env/static/private').npm_config_metrics_registry} */
+export const npm_config_metrics_registry = "https://registry.npmjs.org/";
+
+/** @type {import('$env/static/private').npm_config_node_gyp} */
+export const npm_config_node_gyp = "C:\\Users\\Tim\\AppData\\Roaming\\npm\\node_modules\\npm\\node_modules\\node-gyp\\bin\\node-gyp.js";
+
+/** @type {import('$env/static/private').npm_config_noproxy} */
+export const npm_config_noproxy = "";
+
+/** @type {import('$env/static/private').npm_config_npm_version} */
+export const npm_config_npm_version = "9.8.1";
+
+/** @type {import('$env/static/private').npm_config_prefix} */
+export const npm_config_prefix = "C:\\Users\\Tim\\AppData\\Roaming\\npm";
+
+/** @type {import('$env/static/private').npm_config_userconfig} */
+export const npm_config_userconfig = "C:\\Users\\Tim\\.npmrc";
+
+/** @type {import('$env/static/private').npm_config_user_agent} */
+export const npm_config_user_agent = "npm/9.8.1 node/v18.15.0 win32 x64 workspaces/false";
+
+/** @type {import('$env/static/private').npm_execpath} */
+export const npm_execpath = "C:\\Users\\Tim\\AppData\\Roaming\\npm\\node_modules\\npm\\bin\\npm-cli.js";
+
+/** @type {import('$env/static/private').npm_lifecycle_event} */
+export const npm_lifecycle_event = "dev";
+
+/** @type {import('$env/static/private').npm_lifecycle_script} */
+export const npm_lifecycle_script = "vite dev";
+
+/** @type {import('$env/static/private').npm_node_execpath} */
+export const npm_node_execpath = "C:\\Program Files\\nodejs\\node.exe";
+
+/** @type {import('$env/static/private').npm_package_json} */
+export const npm_package_json = "C:\\Users\\Tim\\Documents\\GitHub\\tim-omalley-portfolio\\package.json";
+
+/** @type {import('$env/static/private').npm_package_name} */
+export const npm_package_name = "tim-omalley-portfolio";
+
+/** @type {import('$env/static/private').npm_package_version} */
+export const npm_package_version = "1.0.0";
+
+/** @type {import('$env/static/private').NPM_PREFIX_NPM_CLI_JS} */
+export const NPM_PREFIX_NPM_CLI_JS = "C:\\Users\\Tim\\AppData\\Roaming\\npm\\node_modules\\npm\\bin\\npm-cli.js";
+
+/** @type {import('$env/static/private').NUMBER_OF_PROCESSORS} */
+export const NUMBER_OF_PROCESSORS = "20";
+
+/** @type {import('$env/static/private').OneDrive} */
+export const OneDrive = "C:\\Users\\Tim\\OneDrive";
+
+/** @type {import('$env/static/private').OneDriveConsumer} */
+export const OneDriveConsumer = "C:\\Users\\Tim\\OneDrive";
+
+/** @type {import('$env/static/private').ORIGINAL_XDG_CURRENT_DESKTOP} */
+export const ORIGINAL_XDG_CURRENT_DESKTOP = "undefined";
+
+/** @type {import('$env/static/private').OS} */
+export const OS = "Windows_NT";
+
+/** @type {import('$env/static/private').Path} */
+export const Path = "C:\\Users\\Tim\\Documents\\GitHub\\tim-omalley-portfolio\\node_modules\\.bin;C:\\Users\\Tim\\Documents\\GitHub\\node_modules\\.bin;C:\\Users\\Tim\\Documents\\node_modules\\.bin;C:\\Users\\Tim\\node_modules\\.bin;C:\\Users\\node_modules\\.bin;C:\\node_modules\\.bin;C:\\Users\\Tim\\AppData\\Roaming\\npm\\node_modules\\npm\\node_modules\\@npmcli\\run-script\\lib\\node-gyp-bin;C:\\Program Files (x86)\\NVIDIA Corporation\\PhysX\\Common;C:\\Python311\\Scripts\\;C:\\Python311\\;C:\\WINDOWS\\system32;C:\\WINDOWS;C:\\WINDOWS\\System32\\Wbem;C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\;C:\\WINDOWS\\System32\\OpenSSH\\;C:\\Program Files\\nodejs\\;C:\\ProgramData\\chocolatey\\bin;C:\\Program Files\\Microsoft SQL Server\\130\\Tools\\Binn\\;C:\\Program Files\\Microsoft SQL Server\\150\\Tools\\Binn\\;C:\\Program Files\\Microsoft SQL Server\\Client SDK\\ODBC\\170\\Tools\\Binn\\;C:\\Program Files\\dotnet\\;C:\\Program Files\\Git\\cmd;C:\\Users\\Tim\\AppData\\Local\\Microsoft\\WindowsApps;C:\\Users\\Tim\\AppData\\Local\\Programs\\Microsoft VS Code\\bin;C:\\Users\\Tim\\AppData\\Roaming\\npm;C:\\Users\\Tim\\AppData\\Local\\GitHubDesktop\\bin;C:\\Users\\Tim\\.dotnet\\tools";
+
+/** @type {import('$env/static/private').PATHEXT} */
+export const PATHEXT = ".COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC;.PY;.PYW";
+
+/** @type {import('$env/static/private').PROCESSOR_ARCHITECTURE} */
+export const PROCESSOR_ARCHITECTURE = "AMD64";
+
+/** @type {import('$env/static/private').PROCESSOR_IDENTIFIER} */
+export const PROCESSOR_IDENTIFIER = "Intel64 Family 6 Model 154 Stepping 3, GenuineIntel";
+
+/** @type {import('$env/static/private').PROCESSOR_LEVEL} */
+export const PROCESSOR_LEVEL = "6";
+
+/** @type {import('$env/static/private').PROCESSOR_REVISION} */
+export const PROCESSOR_REVISION = "9a03";
+
+/** @type {import('$env/static/private').ProgramData} */
+export const ProgramData = "C:\\ProgramData";
+
+/** @type {import('$env/static/private').ProgramFiles} */
+export const ProgramFiles = "C:\\Program Files";
+
+/** @type {import('$env/static/private').ProgramW6432} */
+export const ProgramW6432 = "C:\\Program Files";
+
+/** @type {import('$env/static/private').PROMPT} */
+export const PROMPT = "$P$G";
+
+/** @type {import('$env/static/private').PSModulePath} */
+export const PSModulePath = "C:\\Program Files\\WindowsPowerShell\\Modules;C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\Modules";
+
+/** @type {import('$env/static/private').PUBLIC} */
+export const PUBLIC = "C:\\Users\\Public";
+
+/** @type {import('$env/static/private').SESSIONNAME} */
+export const SESSIONNAME = "Console";
+
+/** @type {import('$env/static/private').SystemDrive} */
+export const SystemDrive = "C:";
+
+/** @type {import('$env/static/private').SystemRoot} */
+export const SystemRoot = "C:\\WINDOWS";
+
+/** @type {import('$env/static/private').TEMP} */
+export const TEMP = "C:\\Users\\Tim\\AppData\\Local\\Temp";
+
+/** @type {import('$env/static/private').TERM_PROGRAM} */
+export const TERM_PROGRAM = "vscode";
+
+/** @type {import('$env/static/private').TERM_PROGRAM_VERSION} */
+export const TERM_PROGRAM_VERSION = "1.81.1";
+
+/** @type {import('$env/static/private').TMP} */
+export const TMP = "C:\\Users\\Tim\\AppData\\Local\\Temp";
+
+/** @type {import('$env/static/private').USERDOMAIN} */
+export const USERDOMAIN = "LAPTOP-H4STDHO7";
+
+/** @type {import('$env/static/private').USERDOMAIN_ROAMINGPROFILE} */
+export const USERDOMAIN_ROAMINGPROFILE = "LAPTOP-H4STDHO7";
+
+/** @type {import('$env/static/private').USERNAME} */
+export const USERNAME = "Tim";
+
+/** @type {import('$env/static/private').USERPROFILE} */
+export const USERPROFILE = "C:\\Users\\Tim";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_EXTRA_ARGS} */
+export const VSCODE_GIT_ASKPASS_EXTRA_ARGS = "--ms-enable-electron-run-as-node";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_MAIN} */
+export const VSCODE_GIT_ASKPASS_MAIN = "c:\\Users\\Tim\\AppData\\Local\\Programs\\Microsoft VS Code\\resources\\app\\extensions\\git\\dist\\askpass-main.js";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_NODE} */
+export const VSCODE_GIT_ASKPASS_NODE = "C:\\Users\\Tim\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe";
+
+/** @type {import('$env/static/private').VSCODE_GIT_IPC_HANDLE} */
+export const VSCODE_GIT_IPC_HANDLE = "\\\\.\\pipe\\vscode-git-a63de41d48-sock";
+
+/** @type {import('$env/static/private').windir} */
+export const windir = "C:\\WINDOWS";
+
+/** @type {import('$env/static/private').ZES_ENABLE_SYSMAN} */
+export const ZES_ENABLE_SYSMAN = "1";
